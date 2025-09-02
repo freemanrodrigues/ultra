f803052825ed10a700b1f53316bcd3cb233a0fd4
Freeman Rodrigues <freeman.rodrigues@gmail.com>
2025-09-02 16:03:42 +0530

    Remove the duplicate sample link

--- resources/views/layouts/side-navbar.blade.php
+++ resources/views/layouts/side-navbar.blade.php
@@ -324,11 +324,6 @@
 </a>
 </li>
 <li class="nav-item">
-<a href="{{ route('sample-oil-type.index') }}" class="nav-link">                  
-<i class="nav-icon bi bi-circle"></i><p>Type of Sample</p>
-</a>
-</li>
-<li class="nav-item">
 <a href="{{ route('sample-type.index') }}" class="nav-link">                  
 <i class="nav-icon bi bi-circle"></i><p>Type of Sample</p>
 </a>
